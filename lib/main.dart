13701d12760eb0fe19e94eb5b1aae2959b040805
FedericoPR <federico.pernas@alumnos.uva.es>
2020-10-14 10:06:35 +0200

    Click y cambio de estado

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
-class MyApp extends StatelessWidget {
+var tarea = { "nombre": "Tarea uno", "done": false};
+void main() => runApp(Tarea());
+class Tarea extends StatefulWidget{
+  @override
+  State<Tarea> createState() {
+    print("Crear state");
+    return new TareaState();
+  }
+}
+
+class TareaState extends State<Tarea> {
+
+  void _click(){
+    print("Click");
+    setState( () {
+      tarea['done'] = !tarea['done'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
+          appBar: AppBar(
+            title: Text('Welcome to Flutter'),
+          ),
+          body: Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Row(
+                children: [
+                  if(tarea['done']) Icon(Icons.done)
+                  else Icon(Icons.clear),
+                  FlatButton(
+                      onPressed: (){_click();},
+                      child:Text(tarea['nombre'])
+                  )
+                ]
+            ),
+          )
       ),
     );
   }
