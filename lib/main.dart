a6ed26e877557c0b5870bb19d2171ea9f504600c
FedericoPR <federico.pernas@alumnos.uva.es>
2020-10-14 09:41:03 +0200

    Comienzo desde cero

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,6 @@
 import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
-
-
-//------------------------------------------
-class Tarea extends StatefulWidget {
-  var laTarea;
-  // constructor
-  Tarea( t ){
-    print(" constructor de la tarea"+ t['nombre']);
-    this.laTarea=t;
-  }
-
-  @override
-  State<Tarea> createState() {
-    print("Create una tarea");
-    return new TareaState(laTarea);
-  }
-}
-
-class TareaState extends State<Tarea>{
-  var tst;
-  TareaState(ts){
-    this.tst = ts;
-  }
-
-  click(){
-    print("click");
-    setState(() {
-      tst['done'] = !tst['done'];
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.only(left:20),
-        child: Row(
-          children: [
-            if (tst['done'])
-              Icon(Icons.done)
-            else
-              Icon(Icons.clear),
-            FlatButton(
-              onPressed: click,
-              child: Text("  "+tst['nombre']),
-            )
-          ],
-        )
-
-        //Text(tst["nombre"].toString() )
-    );
-  }
-}
-
-
-//------------------------------------------
 class MyApp extends StatelessWidget {
-
-  var tareaejemplo = {"nombre": "Hola soy una tarea", "done": false};
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -68,26 +10,7 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Container(
-              padding: EdgeInsets.all(30),
-              decoration: BoxDecoration(
-                color: Colors.lightGreenAccent,
-              ),
-              child: Row(
-                children: [
-                  Tarea(tareaejemplo),//!!!------
-                  /*
-
-                  if (tarea['done'])
-                    Icon(Icons.done)
-                  else
-                    Icon(Icons.clear),
-                   Text(tarea['nombre']),
-
-                   */
-                ],
-              )
-          )
+          child: Text('Hello World'),
         ),
       ),
     );
